ae8b7daf444b9e2f29470ea3fcd469b8edfe9792
Andy Loftus <aloftus@gmail.com>
2022-07-28 18:37:36 +0000

    make consumer & provider

---
+++ manifests/provider/ldifs.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile_ldap_server::provider::ldifs
+class profile_ldap_server::provider::ldifs {
+}
