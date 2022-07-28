71bdf06f52f96b120b697d923fbb74269c2b711b
Andy Loftus <aloftus@gmail.com>
2022-07-28 18:38:33 +0000

    remove tcpwrappers

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,13 +9,6 @@ class profile_ldap_server {
   include profile_ldap_server::firewall
   include profile_ldap_server::services
 
-  # tcpwrappers was removed in RHEL8
-  $os_family = $facts['os']['family']
-  $os_rel_major = Integer( $facts['os']['release']['major'] )
-  if $os_family == 'RedHat' and $os_rel_major < 8 {
-    include ::profile_ldap_server::tcpwrappers
-  }
-
   Class['epel']
     -> Class['profile_ldap_server::services']
 
